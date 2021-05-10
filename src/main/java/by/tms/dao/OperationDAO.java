488a54ce37f59c0606bd44a63cb83bb9bf040a55
Anton Novosad <noobito.2011@gmail.com>
2021-05-10 22:55:00 +0300

    rename method

--- src/main/java/by/tms/dao/OperationDAO.java
+++ src/main/java/by/tms/dao/OperationDAO.java
@@ -7,5 +7,6 @@ import java.util.List;
 
 public interface OperationDAO {
     void saveOperation(Operation operation);
-    List<Operation> listOperation(User user);
+
+    List<Operation> getListOperation();
 }
