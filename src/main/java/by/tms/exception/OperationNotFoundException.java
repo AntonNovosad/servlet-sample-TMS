0d367003d6e8cf9ba33181f0b2e013841a1cc16d
Anton Novosad <noobito.2011@gmail.com>
2021-05-10 22:54:17 +0300

    added exception class

---
+++ src/main/java/by/tms/exception/OperationNotFoundException.java
@@ -0,0 +1,10 @@
+package by.tms.exception;
+
+public class OperationNotFoundException extends Exception {
+    public OperationNotFoundException() {
+    }
+
+    public OperationNotFoundException(String message) {
+        super(message);
+    }
+}
